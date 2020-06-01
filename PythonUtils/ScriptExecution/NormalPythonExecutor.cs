884346bbdad4fff7e1cba60cac8ebca77dd97633
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2020-06-01 08:22:35 +0200

    Prevent crash of MR when no script exists

--- PythonUtils/ScriptExecution/NormalPythonExecutor.cs
+++ PythonUtils/ScriptExecution/NormalPythonExecutor.cs
@@ -15,10 +15,19 @@ namespace PythonUtils.ScriptExecution
             {
                 if (string.IsNullOrEmpty(this.script) || this.source == null || !this.script.Equals(value))//empty or changed
                 {
-                    this.script = value;
-                    this.script = this.script.Replace("\r", "");
-                    //RECO the scope should be reset too!
-                    this.source = engine.CreateScriptSourceFromString(this.script, Microsoft.Scripting.SourceCodeKind.AutoDetect);
+                    script = value;
+
+                    if (script != null)
+                    {
+                        script = script.Replace("\r", "");
+                        //RECO the scope should be reset too!
+                        source = engine.CreateScriptSourceFromString(this.script,
+                            Microsoft.Scripting.SourceCodeKind.AutoDetect);
+                    }
+                    else
+                    {
+                        source = null;
+                    }
                 }
             }
         }
