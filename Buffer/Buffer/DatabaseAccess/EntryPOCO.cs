f9136dae2055e0a6283b647c4c3acbc9249c7144
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2021-06-06 14:48:49 +0200

    Modularize database access
    Move database access logic out of the `OutputLoop` class

---
+++ Buffer/Buffer/DatabaseAccess/EntryPOCO.cs
@@ -0,0 +1,26 @@
+﻿using Model.Variables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buffer.DatabaseAccess
+{
+    public class EntryPOCO
+    {
+        public int GlobalCounter { set; get; }
+        public int StartCounterOfScansOfCurrentModel { set; get; }
+        public int StartGlobalCounterOfMeasurementRoutine { set; get; }
+        public int IterationOfScan { set; get; }
+        public int CompletedScans { set; get; }
+        public int NumberOfIterations { set; get; }
+        public double CycleDuration { set; get; }
+        public bool IsMeasurementRoutineMode { set; get; }
+        public bool IsIterating { set; get; }
+        public int ModelIndex { set; get; }
+
+        public List<VariableModel> VariableList { set; get; }
+        public DateTime EstimatedStartTime { set; get; }
+    }
+}
